235899b68a775dd7bc4efb5e0bceb99371c98bc8
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2022-07-16 23:07:54 -0400

    added axios call to get warehouse list and mapped the response

--- src/pages/Warehouses/Warehouses.js
+++ src/pages/Warehouses/Warehouses.js
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react';
+import axios from 'axios';
 
-const Warehouses = (props) => {
-    return (
-        <div>
-            <h1>Warehouses</h1>
-            <form>
-                <input placeholder='Search'></input>
-                <button>+ Add New Warehouse</button>
-            </form>
-            <p>WAREHOUSE</p>
-            <p>ADDRESS</p>
-            <p>CONTACT NAME</p>
-            <p>CONTACT INFORMATION</p>
-        </div>
-    )
-}
+const apiUrl = "http://localhost:8080"
+
+class Warehouses extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            warehouseList: [],
+            warehouseId: '',
+        }
+    }
+    componentDidMount() {
+        axios.get(`${apiUrl}/warehouses`)
+        .then((response) => {
+            console.log(response.data)
+            this.setState({
+                warehouseList: response.data
+            });
+        }).catch((error) => {
+            console.log('Error')
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Warehouses</h1>
+                <form>
+                    <input placeholder='Search'></input>
+                    <button>+ Add New Warehouse</button>
+                </form>
+                <div>
+                    {this.state.warehouseList.map((warehouseNames)=>{
+                        return(
+                            <div>
+                                <p>WAREHOUSE</p>
+                                <p>{warehouseNames.name}</p>
+                                <p>ADDRESS</p>
+                                <p>{warehouseNames.address}, {warehouseNames.city}, {warehouseNames.country}</p>
+                                <p>CONTACT NAME</p>
+                                <p>{warehouseNames.contact.name}</p>
+                                <p>CONTACT INFORMATION</p>
+                                <p>{warehouseNames.contact.phone}</p>
+                                <p>{warehouseNames.contact.email}</p>
+                            </div>
+                        )
+                    })
+                    }
+                </div>
+            </div>
+        )
+    }
+    
+};
+
 
 export default Warehouses;
