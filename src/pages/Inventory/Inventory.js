07ccefd5fb0ff1aa91cae3d0704cba003fa0d198
HaveNoLimit <93959590+HaveNoLimit@users.noreply.github.com>
2022-07-22 14:01:15 -0400

    inventory list front end seems to be working fine pulling the info from api

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import React, { Component } from "react";
-// import ReactDOM from "react";
+import React, { Component } from "react";
+import ReactDOM from "react";
 import axios from "axios";
-// import InventoryItemDetails from "../../components/InventoryItemDetails/InventoryItemDetails";
+import InventoryItemDetails from "../../components/InventoryItemDetails/InventoryItemDetails";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 
