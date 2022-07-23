d66de4e72fd34ffbcc86a34dfcb600a8871e7984
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2022-07-22 22:46:18 -0400

    styling added for mobile

--- src/pages/Warehouses/Warehouses.js
+++ src/pages/Warehouses/Warehouses.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import deleteIcon from '../../assets/icons/delete_outline-24px.svg';
 import editIcon from '../../assets/icons/edit-24px.svg';
 import './Warehouses.scss';
+import { Link } from 'react-router-dom';
 
 
 const apiUrl = "http://localhost:8080"
@@ -31,29 +32,33 @@ class Warehouses extends React.Component {
     render() {
         return (
             <div className='warehouses'>
-                <h1 className='warehouses__title'>Warehouses</h1>
-                <form className='warehouses__form'>
-                    <input placeholder='Search'></input>
-                    <button>+ Add New Warehouse</button>
-                </form>
-                <div>
+                <div className='warehouses__container'>
+                    <h1 className='warehouses__title'>Warehouses</h1>
+                    <form className='warehouses__form'>
+                        <input className='warehouses__form--input' placeholder='Search'></input>
+                        <button className='warehouses__form--button'>+ Add New Warehouse</button>
+                    </form>
+                </div>
+                <div className='warehosue__container'>
                     {this.state.warehouseList.map((warehouseNames)=>{
                         return(
                             <div className='warehouse'>
                                 <div className='warehouse__left'>
-                                    <h2>WAREHOUSE</h2>
-                                    <p>{warehouseNames.name}</p>
-                                    <h2>ADDRESS</h2>
-                                    <p>{warehouseNames.address}, {warehouseNames.city}, {warehouseNames.country}</p>
-                                    <img src={deleteIcon}></img>
+                                    <h2 className='warehouse__title'>WAREHOUSE</h2>
+                                    <Link to={`/warehouses/${warehouseNames.id}`}>
+                                        <p className='warehouse__text'>{warehouseNames.name} > </p>
+                                    </Link>
+                                    <h2 className='warehouse__title'>ADDRESS</h2>
+                                    <p className='warehouse__text'>{warehouseNames.address}, {warehouseNames.city}, {warehouseNames.country}</p>
+                                    <img className='warehouse__image' src={deleteIcon}></img>
                                 </div>
-                                <div className='warehouse__left'>
-                                    <h2>CONTACT NAME</h2>
-                                    <p>{warehouseNames.contact.name}</p>
-                                    <h2>CONTACT INFORMATION</h2>
-                                    <p>{warehouseNames.contact.phone}</p>
-                                    <p>{warehouseNames.contact.email}</p>
-                                    <img src={editIcon}></img>
+                                <div className='warehouse__right'>
+                                    <h2 className='warehouse__title'>CONTACT NAME</h2>
+                                    <p className='warehouse__text'>{warehouseNames.contact.name}</p>
+                                    <h2 className='warehouse__title'>CONTACT INFORMATION</h2>
+                                    <p className='warehouse__text'>{warehouseNames.contact.phone}</p>
+                                    <p className='warehouse__text'>{warehouseNames.contact.email}</p>
+                                    <img className='warehouse__image--edit' src={editIcon}></img>
                                 </div>
                             </div>
                         )
