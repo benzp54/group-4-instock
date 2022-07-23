179f01dc69dcab05ce5c9f3c93d619d9ad67606a
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2022-07-23 12:01:34 -0400

    section tag added

--- src/components/EditInventoryItem/EditInventoryItem.js
+++ src/components/EditInventoryItem/EditInventoryItem.js
@@ -4,9 +4,11 @@ import arrowBack from '../../assets/icons/arrow_back-24px.svg';
 function EditInventory() {
 
     return(
-        <div className="editInventory__heading">
-                <img src={arrowBack} alt="back arrow" className="icon__arrowBack"/>
-                <h1 className="editInventory__title">Edit Inventory Item</h1>
-        </div>
+        <section>
+            <div className="editInventory__heading">
+                    <img src={arrowBack} alt="back arrow" className="icon__arrowBack"/>
+                    <h1 className="editInventory__title">Edit Inventory Item</h1>
+            </div>
+        </section>
     )
 }
