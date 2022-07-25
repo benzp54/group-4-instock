e0a25a5b37c296d64e0cb1638085161fd40a1d87
DenverRepositini <denverreghelini@gmail.com>
2022-07-24 23:36:31 -0600

    Repushing addNewWarehouse branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
-// import Header from "./components/Header/Header";
-import Warehouse from "./pages/Warehouses/Warehouses";
-// import Inventory from "./pages/Inventory/Inventory";
-// import Footer from "./components/Footer/Footer";
-import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Warehouses from "./pages/Warehouses/Warehouses";
+import Inventory from "./pages/Inventory/Inventory";
+import Footer from "./components/Footer/Footer"
 
 class App extends React.Component {
   render() {
@@ -16,20 +12,14 @@ class App extends React.Component {
         <BrowserRouter>
           {/* <Header /> */}
           <Switch>
-            <Route path="/" exact component={Warehouse} />
-
-            <Route path="/warehouse" exact component={Warehouse} />
-            <Route path="/warehouse/:id" exact component={WarehouseDetails} />
-            <Route path="/warehouse/add-new-warehouse" component={Warehouse} />
-            <Route path="/warehouse/edit-warehouse" component={Warehouse} />
+            <Route path="/" exact component={Warehouses} />
+            <Route path="/warehouse" exact component={Warehouses}/>
             <Route path="/inventory" component={Inventory} />
-            <Route path="/inventory/:id" component={Inventory} />
-
             <Route
               component={() => alert("(404) Bad Request, Page Not Found")}
             />
           </Switch>
-          {/* <Footer /> */}
+          <Footer />
         </BrowserRouter>
       </div>
     );
