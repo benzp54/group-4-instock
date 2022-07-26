38bf2e606e803c96b5cfb17de4f1ce8896f2e144
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2022-07-25 22:29:50 -0400

    header content added

--- src/pages/Warehouses/Warehouses.js
+++ src/pages/Warehouses/Warehouses.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import WarehouseList from "../../components/WarehouseList/WarehouseList"
 import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
 import EditWarehouse from "../../components/EditWarehouse/EditWarehouse";
@@ -9,16 +9,18 @@ class Warehouses extends React.Component{
   render(){
     return(
       <div>
-        <Switch>
-          <Route path="/" exact component={WarehouseList} />
-          <Route path="/warehouse/:id" component={WarehouseDetails} />
-          <Route path="/warehouse/edit/:id" component={EditWarehouse} />
-          <Route path="/warehouse/add" component={AddNewWarehouse} />
-          <Route />
-          {/* <Route path="/warehouse" exact component={Warehouses} />
-            <Route path="/warehouse/:id" exact component={WarehouseDetails} />
-            <Route path="/inventory/:id" component={Inventory} /> */}
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={WarehouseList} />
+            <Route path="/warehouse/:id" component={WarehouseDetails} />
+            <Route path="/warehouse/edit/:id" component={EditWarehouse} />
+            <Route path="/warehouse/add" component={AddNewWarehouse} />
+            <Route />
+            {/* <Route path="/warehouse" exact component={Warehouses} />
+              <Route path="/warehouse/:id" exact component={WarehouseDetails} />
+              <Route path="/inventory/:id" component={Inventory} /> */}
+          </Switch>
+        </BrowserRouter>
       </div>  
     );
   }
